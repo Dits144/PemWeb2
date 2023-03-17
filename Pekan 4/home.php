b40ade2861f6f80802c095a19550b5e3da83ebea
Dits144 <Dits144@gmail.com>
2023-03-17 15:30:21 +0700

    3

---
+++ Pekan 4/home.php
@@ -0,0 +1,39 @@
+<?php
+    include_once 'template/header.php';
+?>
+<table class="table table-striped">
+
+<thead>
+    <tr class="bg-primary">
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr class="table-primary">
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr class="table-secondary">
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr class="table-success">
+      <th scope="row">3</th>
+      <td>Larry</td>
+      <td>the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+
+<?php
+    include_once 'template/footer.php';
+?>
+
